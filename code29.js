8f167a09fc0bbabc5a5665075ab476973ca5a084
Hannah Barton <hannbarton@gmail.com>
2019-02-26 11:03:09 -0500

    code29

---
+++ code29.js
@@ -0,0 +1,22 @@
+// given two strings, see if 1 is a permuation of the other
+
+const isPerm = (one, two) => {
+
+    // sort the strings first;
+
+    let str1 = one.split("").sort()
+    let str2 = two.split("").sort()
+
+    for (let i = 0; i < str1.length;  i++) {
+        if (str1[i] !== str2[i]) {
+            return false
+        }
+    }
+    return true;
+}
+
+// console.log(isPerm('tacocat', 'catcato'))
+
+(function call(){
+    console.log('this worked')
+})()
